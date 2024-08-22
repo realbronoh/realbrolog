3d914e8abbbc46ef2311392326a37c5f3446a1c8
realbro <realbro.noh@gmail.com>
2024-08-22 07:42:52 +0000

    center article at mobile size

--- src/components/Center.tsx
+++ src/components/Center.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Center = (props: { children: React.ReactNode }) => {
   return (
-    <div className="mx-auto p-4 md:max-w-3xl md:p-8">{props.children}</div>
+    <div className="mx-auto p-4 md:max-w-3xl md:p-8 flex items-center justify-center md:justify-start">
+      {props.children}
+    </div>
   );
 };
 
