f195582b48207d2f9a632b106d635050bcfeacf8
realbro <realbro.noh@gmail.com>
2024-08-26 13:22:47 +0000

    generate sitemap.xml for /home, /posts, and /post/slug

---
+++ src/app/sitemap.ts
@@ -0,0 +1,58 @@
+import { LOCALES } from '@/constants/intl';
+import { BASE_URL } from '@/constants/misc';
+import { getPostManager } from '@/utils/postManager';
+import type { MetadataRoute } from 'next';
+
+const generateBasicLinks = () => {
+  return [
+    {
+      url: `${BASE_URL}/en/home`,
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          ko: `${BASE_URL}/ko/home`,
+        },
+      },
+    },
+    {
+      url: `${BASE_URL}/ko/home`,
+      lastModified: new Date(),
+      alternates: {
+        languages: {
+          en: `${BASE_URL}/en/home`,
+        },
+      },
+    },
+    {
+      url: `${BASE_URL}/en/posts`,
+      lastModified: new Date(),
+      // No alternates here because the contents differ
+    },
+    {
+      url: `${BASE_URL}/ko/posts`,
+      lastModified: new Date(),
+      // No alternates here because the contents differ
+    },
+  ];
+};
+
+const generatePostLinks = () => {
+  const links: { url: string; lastModified: Date }[] = [];
+  LOCALES.forEach((locale) => {
+    const posts = getPostManager().getPostByLocale(locale);
+    posts.forEach((post) => {
+      const link = {
+        url: `${BASE_URL}/${locale}/post/${post.slug}`,
+        lastModified: new Date(),
+      };
+      links.push(link);
+    });
+  });
+  return links;
+};
+
+const sitemap = (): MetadataRoute.Sitemap => {
+  return [...generateBasicLinks(), ...generatePostLinks()];
+};
+
+export default sitemap;
--- src/constants/misc.ts
+++ src/constants/misc.ts
@@ -1,3 +1,4 @@
 export const KST_ZONE = 'Asia/Seoul';
 export const KST_TIME_OFFSET_HOUR = 9;
 export const REALBROLOG_NAME = 'Realbrolog';
+export const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL!;
