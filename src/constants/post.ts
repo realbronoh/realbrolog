41736afabf3b38fcf0dd27d0d4480592cca08c76
realbro <realbro.noh@gmail.com>
2024-09-13 14:58:33 +0000

    change number of posts at /posts page to 8 from 5

--- src/constants/post.ts
+++ src/constants/post.ts
@@ -1,4 +1,4 @@
 export const POSTS_DIR = 'posts';
 export const MARKDOWN_EXT = '.md';
 export const DUMMY_POST_ID = 'dummy_post_id';
-export const DEFAULT_NUM_POSTS_PER_PAGE = 5;
+export const DEFAULT_NUM_POSTS_PER_PAGE = 8;
